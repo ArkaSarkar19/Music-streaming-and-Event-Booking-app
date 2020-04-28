d102f89f01d68882ac58dee5571842558fa18266
Amit Maurya <amitmaurya11042000@gmail.com>
2020-04-28 23:39:36 +0530

    AllEvents and SelectShow

--- src/Database/DataController.java
+++ src/Database/DataController.java
@@ -9,25 +9,26 @@ import java.util.Map;
 public class DataController {
     public Connection connection;
 
-    public List<Map<String,String>> getData(String tableName) throws MyException{
-        List<Map<String,String>> data  = null;
-        if(tableName == null) throw new InvalidTableNameException("Enter a valid table Name");
-        try{
+    public List<Map<String, String>> getData(String tableName) throws MyException {
+        List<Map<String, String>> data = null;
+        if (tableName == null) throw new InvalidTableNameException("Enter a valid table Name");
+        try {
             DBConnection con = new DBConnection();
             connection = con.getConnection();
-            if(connection == null) throw new ConnectionInvalidException("Connection not Establised");
+            if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
             Statement stmt = connection.createStatement();
             List<String> allTables = getAllTables();
-            if(!allTables.contains(tableName.toUpperCase())) throw new InvalidTableNameException(tableName + "is not a table in database");
+            if (!allTables.contains(tableName.toUpperCase()))
+                throw new InvalidTableNameException(tableName + "is not a table in database");
             String query = "Select * from" + tableName.toUpperCase();
             ResultSet rs = stmt.executeQuery(query);
             ResultSetMetaData rsmd = rs.getMetaData();
             int columnsNumber = rsmd.getColumnCount();
-            while(rs.next()){
-                Map<String, String> map = new HashMap<String,String>();
-                for(int i=1;i<columnsNumber;i++){
+            while (rs.next()) {
+                Map<String, String> map = new HashMap<String, String>();
+                for (int i = 1; i < columnsNumber; i++) {
                     String columnName = rsmd.getColumnName(i);
-                    map.put(columnName,rs.getString(i));
+                    map.put(columnName, rs.getString(i));
                 }
                 data.add(map);
             }
@@ -38,12 +39,13 @@ public class DataController {
         }
         return data;
     }
-    public List<String> getAllTables(){
-        List<String> data  = new ArrayList<String>();
-        try{
+
+    public List<String> getAllTables() {
+        List<String> data = new ArrayList<String>();
+        try {
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery("Show tables");
-            while(rs.next()) {
+            while (rs.next()) {
                 data.add(rs.getString(1).toUpperCase());
 
             }
@@ -53,45 +55,43 @@ public class DataController {
         return data;
     }
 
-    public   User validateLogin(String email, String password) throws MyException{
-        try{
+    public User validateLogin(String email, String password) throws MyException {
+        try {
             DBConnection con = new DBConnection();
             connection = con.getConnection();
-            if(connection == null) throw new ConnectionInvalidException("Connection not Establised");
+            if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
             Statement stmt = connection.createStatement();
             String query = "Select * from USER as T  where email = '" + email + "' and '" + password + "' = (select password from USER_AUTH as S where S.user_id = T.user_id )";
             System.out.println(query);
             ResultSet rs = stmt.executeQuery(query);
             rs.last();
 //            System.out.println("number of rows"+ rs.getRow());
-            if(rs.getRow() == 0) throw new InvalidUsernamePassowordException("Invalid USERNAME or PASSWORD");
-            User user = new User(rs.getInt("user_id"),rs.getString("name"),rs.getString("country"),rs.getString("email"),rs.getString("DOB"),rs.getString("gender"));
+            if (rs.getRow() == 0) throw new InvalidUsernamePassowordException("Invalid USERNAME or PASSWORD");
+            User user = new User(rs.getInt("user_id"), rs.getString("name"), rs.getString("country"), rs.getString("email"), rs.getString("DOB"), rs.getString("gender"));
 
             connection.close();
             System.out.println("Login");
             return user;
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public void checkUser(String name, String email) throws  MyException{
-        try{
+    public void checkUser(String name, String email) throws MyException {
+        try {
             DBConnection con = new DBConnection();
             connection = con.getConnection();
-            if(connection == null) throw new ConnectionInvalidException("Connection not Establised");
+            if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
             Statement stmt = connection.createStatement();
-            String query = "Select COUNT(*) from USER where binary email = '" + email +"'";
+            String query = "Select COUNT(*) from USER where binary email = '" + email + "'";
             System.out.println(query);
             ResultSet rs = stmt.executeQuery(query);
             rs.next();
-            if(rs.getInt("COUNT(*)") == 1) throw new UserExistsException("User Exists");
+            if (rs.getInt("COUNT(*)") == 1) throw new UserExistsException("User Exists");
             connection.close();
 
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
@@ -99,9 +99,9 @@ public class DataController {
     public void addUser(User user, UserAuth userAuth) throws MyException, SQLException {
         DBConnection con = new DBConnection();
         connection = con.getConnection();
-        if(connection == null) throw new ConnectionInvalidException("Connection not Establised");
+        if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
         Statement stmt = connection.createStatement();
-        String query = "insert into USER values("+user.getUser_id()+",'" + user.getName() + "','" + user.getCountry() + "','" + user.getEmail() + "','" + user.getDOB() + "','" + user.getGender() + "')";
+        String query = "insert into USER values(" + user.getUser_id() + ",'" + user.getName() + "','" + user.getCountry() + "','" + user.getEmail() + "','" + user.getDOB() + "','" + user.getGender() + "')";
         System.out.println(query);
         stmt.executeUpdate(query);
         query = "insert into USER_AUTH values(" + userAuth.getUser_id() + ",'" + userAuth.getPassword() + "')";
@@ -117,20 +117,51 @@ public class DataController {
             connection = con.getConnection();
             if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
             Statement stmt = connection.createStatement();
-            String query = "insert into USER_PLAYLISTS values(" + userPlaylist.getPlaylist_id() + "," + userPlaylist.getUser_id()  + " ,'" + userPlaylist.getName().toString() + "'," + userPlaylist.getSong_num() + ")";
+            String query = "insert into USER_PLAYLISTS values(" + userPlaylist.getPlaylist_id() + "," + userPlaylist.getUser_id() + " ,'" + userPlaylist.getName().toString() + "'," + userPlaylist.getSong_num() + ")";
             System.out.println(query);
             stmt.executeUpdate(query);
             System.out.println("Successfull");
             connection.close();
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-            throw  new CannotAddPlaylsitException("Cannot add playlist");
+            throw new CannotAddPlaylsitException("Cannot add playlist");
         }
     }
 
+    public ArrayList<ArrayList<String>> ExecuteQuery(String query) throws MyException {
+        ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+
+        try {
+            DBConnection con = new DBConnection();
+            connection = con.getConnection();
+            if (connection == null) throw new ConnectionInvalidException("Connection not Establised");
+            Statement stmt = connection.createStatement();
+
+            List<String> allTables = getAllTables();
+            for (String table : allTables)
+                System.out.println(table);
+
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            while (rs.next()) {
+                ArrayList<String> map = new ArrayList<>();
+                for (int i = 1; i <= columnsNumber; i++) {
+                    map.add(rs.getString(i));
+                }
+                data.add(map);
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public ArrayList<UserPlaylist> getAllPlaylists(User user) throws ConnectionInvalidException, CannotAddPlaylsitException {
-        if(user == null) return  null;
+        if (user == null) return null;
         try {
             ArrayList<UserPlaylist> list = new ArrayList<>();
             DBConnection con = new DBConnection();
@@ -140,21 +171,20 @@ public class DataController {
             String query = "SELECT * FROM USER_PLAYLISTS where user_id = " + user.getUser_id();
             System.out.println(query);
             ResultSet rs = stmt.executeQuery(query);
-            while(rs.next()){
+            while (rs.next()) {
                 int playlist_id = rs.getInt("playlist_id");
-                int user_id  = rs.getInt("user_id");
+                int user_id = rs.getInt("user_id");
                 String name = rs.getString("name");
-                int song_num  = rs.getInt("song_num");
-                UserPlaylist up = new UserPlaylist(user_id, playlist_id,name,song_num);
+                int song_num = rs.getInt("song_num");
+                UserPlaylist up = new UserPlaylist(user_id, playlist_id, name, song_num);
                 list.add(up);
             }
             System.out.println("Successfull");
             connection.close();
             return list;
-        }
-        catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-            throw  new CannotAddPlaylsitException("Error getting all the playlists ");
+            throw new CannotAddPlaylsitException("Error getting all the playlists ");
         }
     }
 }
