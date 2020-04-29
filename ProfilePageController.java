f70232513a1f3227abeb9f1f2f809a890421dd70
Harman Singh <56083207+Harman18284@users.noreply.github.com>
2020-04-29 15:39:25 +0530

    Add files via upload
    Profile page, update details done!

---
+++ ProfilePageController.java
@@ -0,0 +1,92 @@
+package Ui.ProfilePage;
+
+import Core.User;
+import Database.DataController;
+import Ui.SignupLoginMain.LoginBox;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ProfilePageController {
+    public static Stage profilePage;
+
+    @FXML
+    public Label profileUserName;
+
+    @FXML
+    public Label profileUserCountry;
+    @FXML
+    public Label profileUserEmail;
+    @FXML
+    public Label profileUserPrefGenre;
+
+    @FXML
+    public Button editProfileButton;
+
+    @FXML
+    public ImageView profilePic;
+
+    public ArrayList<String> editedData;
+
+    public static Scene scene;
+    private User user;
+    public  void loadWindow(User user) {
+        try {
+            this.user = user;
+            profilePage = new Stage();
+            profilePage.initModality(Modality.APPLICATION_MODAL);
+            Parent profile = FXMLLoader.load(ProfilePageController.class.getResource("ProfilePage.fxml"));
+            scene = new Scene(profile);
+            profilePage.setScene(scene);
+            profilePage.setTitle("Profile");
+            profilePage.setResizable(false);
+            profilePage.show();
+
+        }
+        catch (IOException e){
+            System.out.println("Error while opening profile page");
+            e.printStackTrace();
+        }
+
+//        user.display();
+        profileUserName = (Label)scene.lookup("#profileUserName");
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+        String a  = Integer.toString(user.getAge());
+        profileUserName.setText(user.getName() + " , " + a);
+
+        profileUserCountry = (Label)scene.lookup("#profileUserCountry");
+        profileUserCountry.setText("Country : " + user.getCountry());
+
+        profileUserEmail = (Label)scene.lookup("#profileUserEmail");
+        profileUserEmail.setText("Email : " + user.getEmail());
+
+        editProfileButton = (Button)scene.lookup("#editProfileButton") ;
+        editProfileButton.setOnAction(actionEvent -> {
+            EditProfileController c = new EditProfileController();
+            try {
+                c.loadWindow(user);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+    }
+
+    public void closeWindow(){
+        profilePage.close();
+    }
+
+
+
+}
---
+++ User.java
@@ -0,0 +1,91 @@
+package Core;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Pattern;
+import Exception.*;
+import java.util.regex.Pattern;
+
+public class User{
+    private int user_id;
+    private String name;
+
+    private String country;
+    private String email;
+    private String DOB;
+    private String gender;
+    public User(int user_id, String name, String country, String email, String DOB, String gender){
+        this.user_id = user_id;
+        this.name = name;
+        this.country = country;
+        this.email = email;
+        this.DOB = DOB;
+        this.gender = gender;
+
+    }
+    public void checkFormatting() throws MyException {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+        Pattern pat = Pattern.compile(emailRegex);
+        if(!pat.matcher(email).matches()) throw new InvalidEmailException("Enter a correct email address");
+
+        try {
+            DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
+            df.setLenient(false);
+            df.parse(DOB);
+        }
+        catch (ParseException e){
+            throw new IncorrectDateException("Enter a valid date");
+        }
+
+    }
+    public void display(){
+        System.out.println(user_id + " " + name + " " + country + " " + email + " " + DOB + " " + gender);
+
+    }
+    public int getAge(){
+        return Calendar.DATE;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+}
